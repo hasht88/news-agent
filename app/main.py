e39cca031e367ff863fd1c4ecd819d56f4ea9cf5
Hasht <basharat183@gmail.com>
2026-09-09 12:53:44 +0500

    adding vercel object blob as persistant storage

--- app/main.py
+++ app/main.py
@@ -1,4 +1,4 @@
-import re, os
+import re, os, json
 import httpx
 from bs4 import BeautifulSoup, SoupStrainer
 from fastapi import FastAPI, Request, HTTPException, Form
@@ -6,13 +6,14 @@ from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from pathlib import Path
 import pickle
+from vercel.blob import BlobClient
 from urllib.parse import urljoin
 from app.models import AgentSettings
 from app.storage import load_settings, save_settings
 
-
+client = BlobClient()
 if os.environ.get("VERCEL"):
-    DATA_DIR = Path("/tmp/data")
+    DATA_DIR = Path("data")
 else:
     DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 
@@ -20,7 +21,8 @@ DATA_FILE = DATA_DIR / "links.pkl"
 BASE_DIR = Path(__file__).resolve().parent.parent
 
 def ensure_data_dir():
-    DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
+    if not os.environ.get("VERCEL"):
+        DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
 
 app = FastAPI(
     title="News Curation Agent",
@@ -62,23 +64,53 @@ def crawl(sources, header):
     print("Removing duplicates")
     href_list = [dict(t) for t in {tuple(d.items()) for d in href_list}]
     ensure_data_dir()
-    with open(DATA_FILE, 'wb') as f:
-        pickle.dump(href_list, f)
+    if os.environ.get("VERCEL"):
+        try:
+            client.put(
+                "data/links.json",
+                href_list,
+                access="private",  # or "public" — now required
+                content_type="application/json",
+                overwrite=True)
+        except Exception as e:
+            print(f"Error encountered: {e}")
+    else:
+        try:
+            with open(DATA_FILE, 'wb') as f:
+                pickle.dump(href_list, f)
+        except Exception as e:
+            print(f"Error encountered: {e}")
+
     for index, item in enumerate(href_list):
         item["id"] = index
     return href_list
 
 def load_news_data():
-    try:
-        ensure_data_dir()
-        with open(DATA_FILE, "rb") as f:
-            data = pickle.load(f)
-        for index, item in enumerate(data):
-            item["id"] = index
-        return data
-    except Exception as e:
-        print(f"Error loading candidates: {e}")
-        return []
+    if os.environ.get("VERCEL"):
+        try:
+            client.head("data/settings.json")
+            data = client.get("data/links.json", access='private')
+            print("Loading data from Vercel Object Blob")
+            data = json.loads(data.content)
+            for index, item in enumerate(data):
+                item["id"] = index
+            return data
+
+        except Exception as e:
+            print(f"Error loading candidates: {e}")
+            return []
+
+    else:
+        try:
+            ensure_data_dir()
+            with open(DATA_FILE, "rb") as f:
+                data = pickle.load(f)
+            for index, item in enumerate(data):
+                item["id"] = index
+            return data
+        except Exception as e:
+            print(f"Error loading candidates: {e}")
+            return []
 
 
 templates = Jinja2Templates(directory=str(BASE_DIR / "templates"))
--- app/storage.py
+++ app/storage.py
@@ -1,23 +1,32 @@
 import json, os
+from os import access
 from pathlib import Path
 from typing import List
 from app.models import AgentSettings
+from vercel.blob import BlobClient
+from dotenv import load_dotenv
+load_dotenv(".env.local")
 
 # DATA_FILE = Path(__file__).resolve().parent.parent / "data" / "settings.json"
 
 if os.environ.get("VERCEL"):
-    DATA_DIR = Path("/tmp/data")
+    DATA_DIR = Path("data")
 else:
     DATA_DIR = Path(__file__).resolve().parent.parent / "data"
 
 DATA_FILE = DATA_DIR / "settings.json"
 
+client = BlobClient()
 DEFAULT_URLS: List[str] = []
 
 DEFAULT_KEYWORDS: List[str] = []
 
 def ensure_data_dir():
-    DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
+    if os.environ.get("VERCEL"):
+        pass
+        # client.create_folder(f"{DATA_DIR}", overwrite=False)
+    else:
+        DATA_FILE.parent.mkdir(parents=True, exist_ok=True)
 
 def get_default_settings() -> AgentSettings:
     return AgentSettings(
@@ -27,29 +36,59 @@ def get_default_settings() -> AgentSettings:
 
 def load_settings() -> AgentSettings:
     ensure_data_dir()
-    if not DATA_FILE.exists():
-        default_settings = get_default_settings()
-        save_settings(default_settings)
-        return default_settings
-    
-    try:
-        with open(DATA_FILE, "r", encoding="utf-8") as f:
-            data = json.load(f)
+    if os.environ.get("VERCEL"):
+        try:
+            client.head("data/settings.json")
+            data = client.get("data/settings.json", access='private')
+            print("Loading settings from Vercel Object Blob")
+            data = json.loads(data.content)
             return AgentSettings(
                 sources=data.get("sources", []),
                 keywords=data.get("keywords", [])
             )
-    except Exception as e:
-        print(f"Error loading settings, using defaults: {e}")
-        return get_default_settings()
+        except Exception as e:
+            print(f"Settings does not exist. Using defaults: {e}")
+            default_settings = get_default_settings()
+            save_settings(default_settings)
+            return default_settings
+    else:
+        if not DATA_FILE.exists():
+            default_settings = get_default_settings()
+            save_settings(default_settings)
+            return default_settings
+
+        try:
+            with open(DATA_FILE, "r", encoding="utf-8") as f:
+                data = json.load(f)
+                return AgentSettings(
+                    sources=data.get("sources", []),
+                    keywords=data.get("keywords", [])
+                )
+        except Exception as e:
+            print(f"Error loading settings, using defaults: {e}")
+            return get_default_settings()
 
 def save_settings(settings: AgentSettings) -> bool:
     ensure_data_dir()
-    try:
-        with open(DATA_FILE, "w", encoding="utf-8") as f:
-            json.dump(settings.model_dump(), f, indent=2)
-        return True
-    except Exception as e:
-        print(f"Error saving settings: {e}")
-        return False
+    if os.environ.get("VERCEL"):
+        try:
+            # json.dump(settings.model_dump()
+            client.put(
+                "data/settings.json",
+                settings.model_dump_json(),
+                access="private",  # or "public" — now required
+                content_type="application/json",
+                overwrite=True)
+            return True
+        except Exception as e:
+            print(f"Error saving settings: {e}")
+            return False
+    else:
+        try:
+            with open(DATA_FILE, "w", encoding="utf-8") as f:
+                json.dump(settings.model_dump(), f, indent=2)
+            return True
+        except Exception as e:
+            print(f"Error saving settings: {e}")
+            return False
 
